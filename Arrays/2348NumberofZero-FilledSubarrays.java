924298be48d3bcdb30e8e5aae308270ec753bebc
Hariharan-2007 <harihari172007@gmail.com>
2026-06-14 19:09:34 +0530

    today i solved one more problem 2348. Number of Zero-Filled Subarrays with two different approaches

---
+++ Arrays/2348NumberofZero-FilledSubarrays.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long total = 0;
+        long count =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                count++;
+            }else{
+                if(count==0){
+                    continue;
+                }else{
+                    total+= (count*(count+1)/2);
+                    count=0;
+                }
+            }
+        }
+        if(count!=0){
+            // total+=count;
+                    total+= (count*(count+1)/2);
+        }
+        return total;
+
+    }
+}
