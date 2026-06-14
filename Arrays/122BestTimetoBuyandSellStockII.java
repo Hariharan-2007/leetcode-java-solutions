d3f2f56f0b80fc30e10889211cd3a308a1e1c0f3
Hariharan-2007 <harihari172007@gmail.com>
2026-06-14 13:03:03 +0530

    using ai i found the second solution that was quite simple

--- Arrays/122BestTimetoBuyandSellStockII.java
+++ Arrays/122BestTimetoBuyandSellStockII.java
@@ -1,30 +1,39 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min = prices[0],max=0;
-        int maxProfit=0;
-        int dif=0 ;
-        int totalprofit=0;
-        int profit=0;
+        // int min = prices[0],max=0;
+        // int maxProfit=0;
+        // int dif=0 ;
+        // int totalprofit=0;
+        // int profit=0;
         
-        for(int i=1;i<prices.length;i++){
+        // for(int i=1;i<prices.length;i++){
             
-            if(min>prices[i]||max>prices[i]){
-                profit+=totalprofit;
-                totalprofit=0;
-                maxProfit=0;
-                min = prices[i];
-                max= prices[i];
+        //     if(min>prices[i]||max>prices[i]){
+        //         profit+=totalprofit;
+        //         totalprofit=0;
+        //         maxProfit=0;
+        //         min = prices[i];
+        //         max= prices[i];
                
-            }else{
-                dif = prices[i]-min;
-                maxProfit=Math.max(maxProfit,dif);
-                totalprofit=maxProfit;
-                max= prices[i];
-            }
+        //     }else{
+        //         dif = prices[i]-min;
+        //         maxProfit=Math.max(maxProfit,dif);
+        //         totalprofit=maxProfit;
+        //         max= prices[i];
+        //     }
             
-        }
-        if(totalprofit!=0){
-            profit+=maxProfit;
+        // }
+        // if(totalprofit!=0){
+        //     profit+=maxProfit;
+        // }
+        
+        // return profit;
+
+        int profit=0;
+        for(int i =1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){
+                profit+=prices[i]-prices[i-1];
+            }
         }
         return profit;
     }
