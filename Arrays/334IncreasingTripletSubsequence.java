ed37d8bb62774f8a4354ec47ec5f70259fe4500b
Hariharan-2007 <harihari172007@gmail.com>
2026-06-15 19:17:21 +0530

    this was the approach 2 that i learned using the ai

--- Arrays/334IncreasingTripletSubsequence.java
+++ Arrays/334IncreasingTripletSubsequence.java
@@ -1,36 +1,46 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        long n = nums.length;
-        int first=nums[0],second=Integer.MAX_VALUE,third =0;
-        boolean check = false;
-        boolean check2 = false;
-        // for(int i =0;i<n;i++){
-        //     if(nums[i]<0){
-        //         return false;
+        // long n = nums.length;
+        // int first=nums[0],second=Integer.MAX_VALUE,third =0;
+        // boolean check = false;
+        // boolean check2 = false;
+        // // for(int i =0;i<n;i++){
+        // //     if(nums[i]<0){
+        // //         return false;
+        // //     }
+        // // }
+        // for(int i =1;i<n;i++){
+            
+        //     if(nums[i]<=first){
+        //         // check = false;
+        //         first = nums[i];
+        //     }else{
+        //         if(nums[i]>first&& (!(second<nums[i])||second==Integer.MAX_VALUE)){
+        //             second = nums[i];
+        //         }
+        //         if(second<nums[i]){
+        //                 check = true;
+        //                 break;
+        //         }
+                
+                
         //     }
         // }
-        for(int i =1;i<n;i++){
-            
+        // if(check ){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
+        int first = Integer.MAX_VALUE,second = Integer.MAX_VALUE;
+        for(int i =0;i<nums.length;i++){
             if(nums[i]<=first){
-                // check = false;
                 first = nums[i];
+            }else if( second>= nums[i]){
+                second = nums[i];
             }else{
-                if(nums[i]>first&& (!(second<nums[i])||second==Integer.MAX_VALUE)){
-                    second = nums[i];
-                }
-                if(second<nums[i]){
-                        check = true;
-                        break;
-                }
-                
-                
+                return true;
             }
         }
-        if(check ){
-            return true;
-        }else{
-            return false;
-        }
-        
+        return false;
     }
 }
