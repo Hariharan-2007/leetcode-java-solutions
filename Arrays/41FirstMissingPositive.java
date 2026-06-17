2df24c8d6e2ffcbd0f22b05f11554b6c031e34a5
Hariharan-2007 <harihari172007@gmail.com>
2026-06-17 19:25:11 +0530

    Successfully solved this question and learned how to appoarch the question and learn something new today

--- Arrays/41FirstMissingPositive.java
+++ Arrays/41FirstMissingPositive.java
@@ -1,3 +1,25 @@
-public class 41FirstMissingPositive {
-    
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        
+        for(int i=0;i<n;i++){
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]){
+                int temp = nums[i];
+                int temp2= nums[temp-1];
+                nums[i]=temp2;
+                nums[temp-1]=temp;
+              
+            }
+        }
+        for(int j=0;j<n;j++){
+            
+            // if(1<nums[0]){
+            //     return 1;
+            // }
+            if(nums[j]!=j+1)
+                return j+1;
+
+        }
+        return n+1;
+    }
 }
