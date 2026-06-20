2fb213a2f0b9bb8bf01e63fabf4f30e0e05516bf
Hariharan-2007 <harihari172007@gmail.com>
2026-06-20 20:59:20 +0530

    Approch2 for this problem

--- Arrays/796RotateString.java
+++ Arrays/796RotateString.java
@@ -1,3 +1,50 @@
-public  796RotateString {
-    
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int left=0;
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        int n = s.length();
+        // while(left<n){
+        //     if(s.charAt(left)==goal.charAt(left)){
+        //         left++;
+        //     }
+        //     else{
+                // StringBuilder str = new StringBuilder(s);
+                // char c = str.charAt(0);
+                // str.deleteCharAt(0);
+                // str.append(c);
+                // s=str.toString();
+                // n--;
+                // left=0;
+        //     }
+        // }
+        // if(s.equals(goal)){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
+
+        while(n>0){
+            if(s.equals(goal)){
+                return true;
+            }else{
+                 StringBuilder str = new StringBuilder(s);
+                    char c = str.charAt(0);
+                str.deleteCharAt(0);
+                str.append(c);
+                s=str.toString();
+                n--;
+                
+            }
+        }
+        if(s.equals(goal)){
+            
+                return true;
+            }
+        else{
+            return false;
+        }
+
+    }
 }
