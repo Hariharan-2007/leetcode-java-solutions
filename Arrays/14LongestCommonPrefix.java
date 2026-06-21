0c39735fd0def15c14edf2fadca33a87cc8af6d9
Hariharan-2007 <harihari172007@gmail.com>
2026-06-21 21:16:17 +0530

    apporach 2 for this problem with o(nxm)

--- Arrays/14LongestCommonPrefix.java
+++ Arrays/14LongestCommonPrefix.java
@@ -3,23 +3,43 @@ class Solution {
         if(strs==null||strs.length==0){
             return "";
         }
-        Arrays.sort(strs);
-        int n = strs.length;
-        String str1 = strs[0];
-        String str2 = strs[n-1];
-        StringBuilder st=new StringBuilder();
-        int len = Math.min(str1.length(),str2.length());
+        // Arrays.sort(strs);
+        // int n = strs.length;
+        // String str1 = strs[0];
+        // String str2 = strs[n-1];
+        // StringBuilder st=new StringBuilder();
+        // int len = Math.min(str1.length(),str2.length());
 
-        if(len==0){
-            return "";
-        }
-        for(int i=0;i<len;i++){
-            if(str1.charAt(i)==str2.charAt(i)){
-                st.append(str1.charAt(i));
-            }else{
-                break;
+        // if(len==0){
+        //     return "";
+        // }
+        // for(int i=0;i<len;i++){
+        //     if(str1.charAt(i)==str2.charAt(i)){
+        //         st.append(str1.charAt(i));
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // return st.toString();
+
+        String str = strs[0];
+        for(String s: strs){
+            if(str.length()>s.length()){
+                str = s;
             }
         }
-        return st.toString();
+        int n = str.length();
+        int prefix = str.length();
+        for(String s : strs){
+            int i=0;
+            while(i<prefix && s.charAt(i)==str.charAt(i) ){
+                i++;
+            }
+            prefix=i;
+            if(prefix==0){
+                return "";
+            }
+        }
+        return str.substring(0,prefix);
     }
 }
