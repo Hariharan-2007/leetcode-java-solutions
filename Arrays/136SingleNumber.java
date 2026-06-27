10a534678366ec45c5698d68a8f12d995f689011
Hariharan-2007 <harihari172007@gmail.com>
2026-06-27 21:40:52 +0530

    136. Single Number problem i solved with xor operation

--- Arrays/136SingleNumber.java
+++ Arrays/136SingleNumber.java
@@ -1,3 +1,19 @@
-public class 136SingleNumber {
-    
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        
+        int xorvalue=nums[0];
+        // for(int n : nums){
+        //     if(n<0){
+        //         return -1;
+        //     }
+        // }
+        for(int i=1;i<nums.length;i++){
+            
+            xorvalue= xorvalue^nums[i];
+        }
+        return xorvalue;
+    }
 }
