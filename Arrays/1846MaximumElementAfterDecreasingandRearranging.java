26317aaf2f11180ce26d8548db5240e771de682b
Hariharan-2007 <harihari172007@gmail.com>
2026-06-28 13:11:07 +0530

    solved this daily challenge leetcode problem

--- Arrays/1846MaximumElementAfterDecreasingandRearranging.java
+++ Arrays/1846MaximumElementAfterDecreasingandRearranging.java
@@ -1,3 +1,24 @@
-public class 1846MaximumElementAfterDecreasingandRearranging {
-    
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        int val = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(i>0){
+                if(Math.abs(arr[i]-arr[i-1])<=1){
+                    continue;
+                }else{
+                    arr[i]=arr[i-1]+1;
+                }
+            }else{
+                arr[0]=1;
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for(int n: arr){
+            if(max<n){
+                max = n;
+            }
+        }
+        return max;
+    }
 }
