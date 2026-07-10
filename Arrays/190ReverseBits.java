82ec647dc80cd472237c32bc0ab487b3422a704b
Hariharan-2007 <harihari172007@gmail.com>
2026-07-10 21:57:07 +0530

    today solved 2 medium level problem

--- Arrays/190ReverseBits.java
+++ Arrays/190ReverseBits.java
@@ -1,3 +1,12 @@
-public class 190ReverseBits {
-    
+class Solution {
+    public int reverseBits(int n) {
+        int rev=0;
+        for(int i =0;i<32;i++){
+            int last = n&1;
+            rev=rev<<1;
+            rev=rev|last;
+            n>>=1;
+        }
+        return rev;
+    }
 }
