0d0c2fa24f6f90b03f01d6e379b37251d1f8bbfe
Hariharan-2007 <harihari172007@gmail.com>
2026-07-28 21:59:36 +0530

    this problem was not solved completely, it fails in some cases

---
+++ Arrays/792NumberofMatchingSubsequences.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public boolean isSubsequence(String s,String word){
+
+        int i=0,j=0;
+        while(i<s.length() && j<word.length()){
+            if(s.charAt(i)==word.charAt(j)){
+                j++;
+            }
+            i++;
+        }
+        return j==word.length();
+    }
+    public int numMatchingSubseq(String s, String[] words) {
+        int count =0;
+        for(String word:words){
+            if(isSubsequence(s,word)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
