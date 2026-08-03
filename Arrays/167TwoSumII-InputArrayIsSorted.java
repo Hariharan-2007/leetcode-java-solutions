35b70d097d27785e232c96b2eb8503ca28f515fa
Hariharan-2007 <harihari172007@gmail.com>
2026-08-03 20:41:24 +0530

    solved this problem using two pointer approches

---
+++ Arrays/167TwoSumII-InputArrayIsSorted.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        int left =0;
+        int right = n-1;;
+        for(int i=0;i<numbers.length;i++){
+            if(numbers[left]+numbers[right]>target){
+                right--;
+                continue;
+            }else if(numbers[left]+numbers[right]<target){
+                left++;
+                continue;
+
+            }else if(numbers[left]+numbers[right]==target){
+                return new int[]{left+1,right+1};
+            }
+        }
+        return new int[]{};
+    }
+}
