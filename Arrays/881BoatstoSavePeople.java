98476eb79580deaaa67fac8f570bf3b852d6657a
Hariharan-2007 <harihari172007@gmail.com>
2026-08-07 22:25:05 +0530

    solved this question

---
+++ Arrays/881BoatstoSavePeople.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left =0;
+        int n =people.length;
+        int boats=0;
+        int right = n-1;
+        while(left<=right){
+            if(people[left]+people[right]<=limit){
+                left++;
+            }
+            boats++;
+            right--;
+        }
+        return boats;
+    }
+}
