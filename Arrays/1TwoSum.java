675afb7fca72eb5d96288f3d8cf097441efeecb7
Hariharan-2007 <harihari172007@gmail.com>
2026-08-14 14:23:30 +0530

    solved 2 sum problem

--- Arrays/1TwoSum.java
+++ Arrays/1TwoSum.java
@@ -1,13 +1,28 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         HashMap<Integer,Integer>mps = new HashMap<>();
+//         for(int i=0;i<nums.length;i++){
+//             int complement = target-nums[i];
+//             if(mps.containsKey(target-nums[i])){
+//                 return new int[] {mps.get(complement),i};
+//             }
+//             mps.put(nums[i],i);
+//         }
+//         return new int[] {};
+//     }
+// }
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer>mps = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            int complement = target-nums[i];
-            if(mps.containsKey(target-nums[i])){
-                return new int[] {mps.get(complement),i};
+            int complement=target-nums[i];
+            if(mps.containsKey(complement)){
+                return new int[]{mps.get(complement),i};
             }
             mps.put(nums[i],i);
         }
+
         return new int[] {};
     }
 }
