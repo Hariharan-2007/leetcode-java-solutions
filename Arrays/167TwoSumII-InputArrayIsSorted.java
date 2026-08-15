d5ab06f36d06a42d9a29ed6a820834c76aa076e8
Hariharan-2007 <harihari172007@gmail.com>
2026-08-15 21:47:45 +0530

    solved the two sum-II problem again

--- Arrays/167TwoSumII-InputArrayIsSorted.java
+++ Arrays/167TwoSumII-InputArrayIsSorted.java
@@ -1,21 +1,40 @@
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         int n = numbers.length;
+//         int left =0;
+//         int right = n-1;;
+//         for(int i=0;i<numbers.length;i++){
+//             if(numbers[left]+numbers[right]>target){
+//                 right--;
+//                 continue;
+//             }else if(numbers[left]+numbers[right]<target){
+//                 left++;
+//                 continue;
+
+//             }else if(numbers[left]+numbers[right]==target){
+//                 return new int[]{left+1,right+1};
+                
+//             }
+//         }
+//         return new int[]{};
+//     }
+// }
+
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int n = numbers.length;
-        int left =0;
-        int right = n-1;;
-        for(int i=0;i<numbers.length;i++){
-            if(numbers[left]+numbers[right]>target){
-                right--;
-                continue;
-            }else if(numbers[left]+numbers[right]<target){
-                left++;
-                continue;
-
-            }else if(numbers[left]+numbers[right]==target){
-                return new int[]{left+1,right+1};
+       int left =0;
+       int n = numbers.length;
+       int right= n-1;
+       while(left<right){
+            if(numbers[left]+numbers[right]==target){
+                return new int[] {left+1,right+1};
                 
+            }else if(numbers[left]+numbers[right] < target){
+                left++;
+            }else{
+                right--;
             }
-        }
-        return new int[]{};
+       }
+       return new int[] {};
     }
 }
