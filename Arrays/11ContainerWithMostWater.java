f692a32faa7cc9f259a8489b66a155b9cc437842
Hariharan-2007 <harihari172007@gmail.com>
2026-08-17 21:48:52 +0530

    solved this problem

--- Arrays/11ContainerWithMostWater.java
+++ Arrays/11ContainerWithMostWater.java
@@ -1,20 +1,41 @@
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int left =0;
+//         int n = height.length;
+//         int right =n-1;
+//         int finalval = 0;
+//         while(left<right){
+//             int minval = Math.min(height[left],height[right]);
+//             int width = right-left;
+//             int area = minval*width;
+//             finalval = Math.max(area,finalval);
+//             if(height[left]<height[right]){
+//                 left++;
+//             }else{
+//                 right--;
+//             }
+//         }
+//         return finalval;
+//     }
+// }
 class Solution {
     public int maxArea(int[] height) {
         int left =0;
         int n = height.length;
-        int right =n-1;
-        int finalval = 0;
+        int right = n-1;
+        int areafinal =0;
         while(left<right){
-            int minval = Math.min(height[left],height[right]);
-            int width = right-left;
-            int area = minval*width;
-            finalval = Math.max(area,finalval);
+            int width = right -left;
+            int heights = Math.min(height[left],height[right]);
+            int area =  heights*width;
+            areafinal =Math.max(area,areafinal);
             if(height[left]<height[right]){
                 left++;
             }else{
                 right--;
             }
+
         }
-        return finalval;
+        return areafinal;
     }
 }
