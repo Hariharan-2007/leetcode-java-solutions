8bb12e4efadf6b31bd7f04c2c29e7dfe3ae2ab44
Hariharan-2007 <harihari172007@gmail.com>
2026-08-18 18:34:57 +0530

    solved this problem again

--- Arrays/977SquaresofaSortedArray.java
+++ Arrays/977SquaresofaSortedArray.java
@@ -1,3 +1,34 @@
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         int arr[]= new int[nums.length];
+//         // for(int i =0;i<nums.length;i++){
+           
+//         //     arr[i]= nums[i]*nums[i];
+//         // }
+//         // Arrays.sort(arr);
+//         // return arr;
+        
+//         int n = nums.length;
+//         int left =0;
+//         int right =n-1;
+//         int pos = n-1;
+//         while(left<=right){
+//             int leftsq = nums[left]*nums[left];
+//             int rightsq = nums[right]*nums[right];
+//             if(leftsq<rightsq){
+//                 arr[pos]=rightsq;
+//                 right--;
+//             }else {
+//                 arr[pos]=leftsq;
+//                 left++;
+//             }
+//             pos--;
+//         }
+
+//         return arr;
+//     }
+// }
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int arr[]= new int[nums.length];
@@ -7,7 +38,6 @@ class Solution {
         // }
         // Arrays.sort(arr);
         // return arr;
-        
         int n = nums.length;
         int left =0;
         int right =n-1;
