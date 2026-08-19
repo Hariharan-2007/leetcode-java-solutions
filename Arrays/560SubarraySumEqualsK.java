79a1e1b3bcfa68bc005eb1393d202d20c91c3d00
Hariharan-2007 <harihari172007@gmail.com>
2026-08-19 23:16:02 +0530

    solved

--- Arrays/560SubarraySumEqualsK.java
+++ Arrays/560SubarraySumEqualsK.java
@@ -1,3 +1,27 @@
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+        
+//         HashMap<Integer,Integer>prefix = new HashMap<>();
+//         prefix.put(0,1);
+
+//         int currentsum=0;
+//         int count =0;
+
+//         for(int num:nums){
+//             currentsum+=num;
+
+//             int need = currentsum-k;
+//             if(prefix.containsKey(need)){
+//                 count+=prefix.get(need);
+//             }
+//             prefix.put(currentsum,prefix.getOrDefault(currentsum,0)+1);
+
+//         }
+//         return count;
+//     }
+// }
+
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
         
