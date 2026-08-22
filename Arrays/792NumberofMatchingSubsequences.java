5e275e4b0809eda8c5549bbbe082a9693bba88c3
Hariharan-2007 <harihari172007@gmail.com>
2026-08-22 09:37:42 +0530

    solved this question

--- Arrays/792NumberofMatchingSubsequences.java
+++ Arrays/792NumberofMatchingSubsequences.java
@@ -1,20 +1,32 @@
 class Solution {
 
-    public boolean isSubsequence(String s,String word){
-
-        int i=0,j=0;
-        while(i<s.length() && j<word.length()){
-            if(s.charAt(i)==word.charAt(j)){
-                j++;
+    public boolean isSequence(String word,List<List<Integer>>ls){
+            int currentindex =-1;
+        for(char c:word.toCharArray()){
+            List<Integer>list = ls.get(c-'a');
+            int target = currentindex+1;
+            int insertpoint=Collections.binarySearch(list,target);
+            if(insertpoint<0){
+                insertpoint= -1-(insertpoint);
             }
-            i++;
+            if(insertpoint==list.size()){
+                return false;
+            }
+            currentindex=list.get(insertpoint);
         }
-        return j==word.length();
+        return true;
     }
     public int numMatchingSubseq(String s, String[] words) {
+        List<List<Integer>>ls = new ArrayList<>();
+        for(int i=0;i<26;i++){
+            ls.add(new ArrayList<>());
+        }
+        for(int i=0;i<s.length();i++){
+            ls.get(s.charAt(i)-'a').add(i);
+        }
         int count =0;
         for(String word:words){
-            if(isSubsequence(s,word)){
+            if(isSequence(word,ls)){
                 count++;
             }
         }
