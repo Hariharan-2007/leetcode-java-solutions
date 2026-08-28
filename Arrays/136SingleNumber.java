5be1a2de034a32c487ff9e1a6ce60be050e05377
Hariharan-2007 <harihari172007@gmail.com>
2026-08-28 22:05:47 +0530

    solved this problem

--- Arrays/136SingleNumber.java
+++ Arrays/136SingleNumber.java
@@ -1,19 +1,29 @@
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         if(nums.length==1){
+//             return nums[0];
+//         }
+        
+//         int xorvalue=nums[0];
+//         // for(int n : nums){
+//         //     if(n<0){
+//         //         return -1;
+//         //     }
+//         // }
+//         for(int i=1;i<nums.length;i++){
+            
+//             xorvalue= xorvalue^nums[i];
+//         }
+//         return xorvalue;
+//     }
+// }
+
 class Solution {
     public int singleNumber(int[] nums) {
-        if(nums.length==1){
-            return nums[0];
-        }
-        
-        int xorvalue=nums[0];
-        // for(int n : nums){
-        //     if(n<0){
-        //         return -1;
-        //     }
-        // }
+        int n = nums[0];
         for(int i=1;i<nums.length;i++){
-            
-            xorvalue= xorvalue^nums[i];
+            n^=nums[i];
         }
-        return xorvalue;
+        return n;
     }
 }
