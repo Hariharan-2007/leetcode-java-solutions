8205dcbc1d6a5310527dd8edbb75f2863af2b878
Hariharan-2007 <harihari172007@gmail.com>
2026-09-08 23:12:45 +0530

    solved this

--- Arrays/125ValidPalindrome.java
+++ Arrays/125ValidPalindrome.java
@@ -1,22 +1,44 @@
+// // class Solution {
+// //     public boolean isPalindrome(String s) {
+// //         if(s.isEmpty()){
+// //             return false;
+// //         }
+// //         StringBuilder str = new StringBuilder();
+// //         for(int i=0;i<s.length();i++){
+// //             if(Character.isLetterOrDigit(s.charAt(i))){
+// //                 str.append(Character.toLowerCase(s.charAt(i)));
+// //             }
+// //         }
+// //         String str1 = new String(str.toString());
+// //         if(str1.equals(str.reverse().toString())){
+// //             return true;
+// //         }else{
+// //             return false;
+// //         }
+// //     }
+// // }
 // class Solution {
 //     public boolean isPalindrome(String s) {
-//         if(s.isEmpty()){
-//             return false;
-//         }
 //         StringBuilder str = new StringBuilder();
-//         for(int i=0;i<s.length();i++){
-//             if(Character.isLetterOrDigit(s.charAt(i))){
-//                 str.append(Character.toLowerCase(s.charAt(i)));
+//         for(char c:s.toCharArray()){
+//             if(Character.isLetterOrDigit(c)){
+//                 str.append(Character.toLowerCase(c));
 //             }
 //         }
-//         String str1 = new String(str.toString());
-//         if(str1.equals(str.reverse().toString())){
-//             return true;
-//         }else{
-//             return false;
+//         String st=str.toString();
+//         int left =0;
+//         int right = st.length()-1;
+//         while(left<right){
+//             if(st.charAt(left)!=st.charAt(right)){
+//                 return false;
+//             }
+//             left++;
+//             right--;
 //         }
+//         return true;
 //     }
 // }
+
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder str = new StringBuilder();
@@ -25,11 +47,10 @@ class Solution {
                 str.append(Character.toLowerCase(c));
             }
         }
-        String st=str.toString();
         int left =0;
-        int right = st.length()-1;
+        int right =str.length()-1;
         while(left<right){
-            if(st.charAt(left)!=st.charAt(right)){
+            if(str.charAt(left)!=str.charAt(right)){
                 return false;
             }
             left++;
