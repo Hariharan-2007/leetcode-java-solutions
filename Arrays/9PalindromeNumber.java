f98a86d06befd5aaf551360de7dc4daf8213a7ae
Hariharan-2007 <harihari172007@gmail.com>
2026-09-16 23:08:05 +0530

    solved this

--- Arrays/9PalindromeNumber.java
+++ Arrays/9PalindromeNumber.java
@@ -1,3 +1,19 @@
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         int left =0;
+//         int right = String.valueOf(x).length()-1;
+//         String str = String.valueOf(x);
+//         while(left<right){
+//             if(str.charAt(left)!=str.charAt(right)){
+//                 return false;
+//             }
+//             left++;
+//             right--;
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isPalindrome(int x) {
         int left =0;
